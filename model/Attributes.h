18726c16c269bebb49761d7cb87dc680ae767175
GevorHakobyan <gevorghakobyan609@gmail.com>
2024-09-11 19:49:12 +0400

    Interface started 11.09.2024

---
+++ model/Attributes.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace model {
+    class I_Attributes {
+        public:
+        //setters
+        //getters
+        private:
+        //data members
+    };
+} // namespace model
