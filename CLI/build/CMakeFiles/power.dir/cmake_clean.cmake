14537109d13fc6ced49785b552dee2e6ed2dd701
GevorHakobyan <gevorghakobyan609@gmail.com>
2024-10-08 12:43:45 +0400

    Undefined Behavior

---
+++ CLI/build/CMakeFiles/power.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/power.dir/Controller/main.cpp.o"
+  "CMakeFiles/power.dir/Controller/main.cpp.o.d"
+  "power"
+  "power.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/power.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
