75607c198bd8c9cccf330bd01e87547d40cf97b1
GevorHakobyan <gevorghakobyan609@gmail.com>
2024-11-05 14:30:13 +0400

    05/10/2024

---
+++ build/CMakeFiles/SlideMakerProject.dir/CLI/CommandFactory/ItemInfo.cpp.o.d
@@ -0,0 +1,6 @@
+CMakeFiles/SlideMakerProject.dir/CLI/CommandFactory/ItemInfo.cpp.o: \
+ /home/geveorg/Desktop/Slide_Maker/CLI/CommandFactory/ItemInfo.cpp \
+ /usr/include/stdc-predef.h \
+ /home/geveorg/Desktop/Slide_Maker/CLI/CommandFactory/../../Include/ItemInfo.h \
+ /home/geveorg/Desktop/Slide_Maker/CLI/CommandFactory/../../Include/ItemTypes.h \
+ /home/geveorg/Desktop/Slide_Maker/CLI/CommandFactory/../../Include/I_Attributes.h
---
+++ build/CMakeFiles/SlideMakerProject.dir/Document/Attributes/I_Attributes.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/SlideMakerProject.dir/Document/Attributes/I_Attributes.cpp.o: \
+ /home/geveorg/Desktop/Slide_Maker/Document/Attributes/I_Attributes.cpp \
+ /usr/include/stdc-predef.h \
+ /home/geveorg/Desktop/Slide_Maker/Document/Attributes/../../Include/I_Attributes.h
